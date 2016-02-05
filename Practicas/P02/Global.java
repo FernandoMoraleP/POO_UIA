3c5cbdfe5afe1c376ca25ce150fd22f504325448
Fernando Morales <derox59@hotmail.com>
2016-02-04 21:54:30 -0600

    ACTUALIZACION

---
+++ Practicas/P02/Global.java
@@ -0,0 +1,26 @@
+public class Global {
+
+    public static byte a = 3;
+    public static short b = 321;
+    public static int c = 23982;
+    public static long d = 2345;
+    public static float e = 12.4f;
+    public static double f = 38.8;
+    public static char g = 't';
+    public static String hello = "Hola bienvenido";
+
+    public static void main(String[] args) {
+
+        
+
+        System.out.println("El valor para la variable de tipo byte es:  " + a);
+        System.out.println("El valor para la variable de tipo short es:  " + b);
+        System.out.println("El valor para la variable de tipo int es:  " + c);
+        System.out.println("El valor para la variable de tipo long es:  " + d);
+        System.out.println("El valor para la variable de tipo float es:  " + e);
+        System.out.println("El valor para la variable de tipo double es:  " + f);
+        System.out.println("El valor para la variable de tipo char es:  " + g);
+        System.out.println("El valor para la variable de tipo String es:  " + hello);
+    }
+
+}
---
+++ Practicas/P02/VARIABLES.java
@@ -0,0 +1,24 @@
+public class VARIABLES {
+
+    public static void main(String[] args) {
+
+        byte a = 1;
+        short b = 2;
+        int c = 1244;
+        long d = 12345;
+        float r = 10.0f;
+        double f = 1.2;
+        char g = 'w';
+        String hello = "hola";
+
+        System.out.println("El valor para la variable de tipo byte es:  " + a);
+        System.out.println("El valor para la variable de tipo short es:  " + b);
+        System.out.println("El valor para la variable de tipo int es:  " + c);
+        System.out.println("El valor para la variable de tipo long es:  " + d);
+        System.out.println("El valor para la variable de tipo float es:  " + r);
+        System.out.println("El valor para la variable de tipo double es:  " + f);
+        System.out.println("El valor para la variable de tipo char es:  " + g);
+        System.out.println("El valor para la variable de tipo String es:  " + hello);
+    }
+
+}
