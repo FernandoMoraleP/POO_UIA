d64afb6a4c26f45395c0810cac5a88516bae2bdb
Fernando Morales <derox59@hotmail.com>
2016-05-03 21:32:05 -0500

    Examen2doparcial

---
+++ Ejercicios/Examen2doparcial/CB.java
@@ -0,0 +1,24 @@
+public class CB{
+	public int s,x,y,z;
+		public CB(int x, int y, int z){
+		
+
+}
+
+public void suma (int s, int x, int y, int z){
+	
+	s=x+y+z;
+
+	return s;
+}
+
+}
+
+public class CA extends CB{
+	public CA(int x, int y){
+		a = new CB(1,2,3);
+	super(x, y);
+	}
+
+
+}
